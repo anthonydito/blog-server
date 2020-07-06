b624efa369303a767b3f7b91d0d2d4267664d8cd
Anthony Dito <anthonymdito@gmail.com>
2020-07-05 21:24:21 -0700

    setup the blogs data

--- src/index.js
+++ src/index.js
@@ -6,6 +6,24 @@ const port = 8080;
 
 app.use(cors({origin: "http://localhost:3000"}));
 
+const blogs = [
+    {
+        id: "blog3",
+        text: "This is our third blog",
+        createdAt: new Date()
+    },
+    {
+        id: "blog2",
+        text: "This is our second blog",
+        createdAt: new Date()
+    },
+    {
+        id: "blog1",
+        text: "This is our first blog",
+        createdAt: new Date()
+    }
+]
+
 app.get("/", (req, res) => {
     res.send("We just created our first server!");
 });
