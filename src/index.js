368c70bc644ed48c7595a1d543b94a9f88853162
Anthony Dito <anthonymdito@gmail.com>
2020-12-16 01:02:44 -0800

    chapter end

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import express from "express";
 import cors from "cors";
 import {json} from "body-parser";
+import {MongoClient} from "mongodb";
+
+let database;
+
+MongoClient.connect("mongodb://localhost:27017/", function(err, db) {
+    if (err) throw err;
+    database = db.db("blog");
+    console.log("database initialized!");
+});
 
 const app = express();
 const port = 8080;
@@ -14,18 +23,34 @@ app.get("/", (req, res) => {
     res.send("We just created our first server!");
 });
 
+const handleUserBlogsRequest = (req, res, next) => {
+    database.collection("blogs").find({}).sort({ createdAt: -1 }).toArray((err, results) => {
+        if (err) {
+            next(err)
+        } else {
+            res.json(results)
+        }
+    })
+}
+
 app.get("/blogs", (req, res) => {
     res.json(blogs);
 });
 
-app.post("/create-blog", (req, res) => {
+app.post("/create-blog", (req, res, next) => {
     const newBlog = {
-        id: "blog-" + blogs.length.toString(),
         text: req.body.text,
         createdAt: new Date()
     }
-    blogs.unshift(newBlog);
-    res.json(blogs);
+    database.collection("blogs").insertOne(newBlog, (err) => {
+        if (err) {
+            next(err)
+        } else {
+            handleUserBlogsRequest(req, res, next)
+        }
+    })
 });
 
 app.listen(port, () => console.log(`Blog server listening at http://localhost:${port}`));
+
+
